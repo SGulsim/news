b7418c822bddee7b13dcfe131dbe4fec57b4d737
Gulsim <saralievagulsim@gmail.com>
2025-06-03 00:14:37 +0300

    git init

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,10 @@
+export const formatDate = date => {
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	};
+
+	return date.toLocaleString('en-US', options);
+};
